a01a0f66759db2bda89a8228fa2aa527de517a3a
vickychhetri <specialpower321@gmail.com>
2021-05-08 16:32:57 +0530

    Second attempt after changes index file name

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+app.get('/', function (req, res) {
+    res.sendFile(__dirname+"/index.html")
+  })
+  
+  app.get('/about', function (req, res) {
+    res.sendFile(__dirname+"/About.html")
+  })
+  app.listen(3000, function(){
+      console.log("Server started on 3000");
+  })
